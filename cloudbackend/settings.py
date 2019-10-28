a092cc1f356e8e012d96292d1d56e3c8a52a92c0
root <root@ubuntubackend.us-east4-a.c.djangobackend-255913.internal>
2019-10-28 05:56:00 +0000

    Server Configuration

--- cloudbackend/settings.py
+++ cloudbackend/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'diamondnikhil',
+        'PASSWORD': 'diamondnikhil',
         'HOST': 'localhost',
         'PORT': '3306',
       }
@@ -124,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
--- cloudbackend/wsgi.py
+++ cloudbackend/wsgi.py
@@ -6,11 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
-
-import os
-
+#!/usr/bin/python3
+import os,sys
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append('/var/www/html/LazyDB/cloudbackend')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloudbackend.settings')
 
 application = get_wsgi_application()
